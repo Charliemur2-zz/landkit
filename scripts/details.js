f19143c6a694949970e5ba335f73f34fee8ca223
Charliemur2 <charliemurciamunoz@gmail.com>
2021-04-05 00:29:53 -0500

    create:favorites section for all sizes

--- scripts/details.js
+++ scripts/details.js
@@ -70,12 +70,27 @@ function enableScroll() {
 const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.js-whole-article');
 
+/* GETTING THE FAVORITES IDS */
+function getFavoritesIds() {
+  /* localStorage.removeItem('FAVORITESIDS'); */
+  let favoritesIds = localStorage.getItem('FAVORITESIDS');
+  if (favoritesIds === null) favoritesIds = [];
+  localStorage.setItem('FAVORITESIDS', favoritesIds + JSON.stringify(id));
+  const star = document.querySelector('.js-star');
+  star.classList.add('star--clicked');
+}
+
 const renderDetails = async () => {
   const res = await fetch(`http://localhost:3000/posts/${id}`);
   const post = await res.json();
   
   const template = `
     <div class="whole-post">
+      <div class="star js-favorites" onclick="getFavoritesIds()">
+        <svg width="25" height="25" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path class="star__img js-star"fill-rule="evenodd" clip-rule="evenodd" d="M16 6.20038C16 6.38185 15.875 6.55325 15.75 6.68431L12.2596 10.2533L13.0865 15.2943C13.0962 15.3648 13.0962 15.4253 13.0962 15.4959C13.0962 15.758 12.9808 16 12.7019 16C12.5673 16 12.4327 15.9496 12.3173 15.879L8 13.4997L3.68269 15.879C3.55769 15.9496 3.43269 16 3.29808 16C3.01923 16 2.89423 15.758 2.89423 15.4959C2.89423 15.4253 2.90385 15.3648 2.91346 15.2943L3.74038 10.2533L0.240385 6.68431C0.125 6.55325 0 6.38185 0 6.20038C0 5.89792 0.298077 5.77694 0.538462 5.73661L5.36538 5.00063L7.52885 0.413359C7.61538 0.221802 7.77885 0 8 0C8.22115 0 8.38462 0.221802 8.47115 0.413359L10.6346 5.00063L15.4615 5.73661C15.6923 5.77694 16 5.89792 16 6.20038Z" fill="#B4C2D3"/>
+        </svg>
+      </div>
       <h1 class="whole-post__title">${post.title}</h1>
       <p class="whole-post__synopsis">${post.sinopsys}</p>
       <div class="post__author">
@@ -110,5 +125,7 @@ const renderDetails = async () => {
   `
   container.innerHTML = template;
 }
+
+
 
 window.addEventListener('DOMContentLoaded', () => renderDetails());
